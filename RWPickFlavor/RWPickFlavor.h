45901d41e7dee39ed565553195fdacfe61b99196
Horatio Boothe <hboothe27@outlook.com>
2019-09-16 11:56:56 -0600

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Horatio D Boothe on 9/16/19.
+//  Copyright © 2019 Horatio D Boothe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
